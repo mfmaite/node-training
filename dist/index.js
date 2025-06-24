06e5e86574ff4a62523f30b4c277b5722f9c74ff
Maite <martinezf.maite@gmail.com>
2025-06-24 14:22:32 -0300

    Remove dist folder

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-import express from "express";
-import { RegisterRoutes } from "./build/routes";
-export const app = express();
-RegisterRoutes(app);
--- dist/server.js
+++
@@ -1,5 +0,0 @@
-import { app } from "./index";
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
